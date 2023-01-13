681765c8cae01f7d2a6efe4ec1d1deeed532053e
MuhammedGul34 <muhammedgul.mail@gmail.com>
2023-01-13 10:33:44 +0300

    Creating Project

---
+++ SimpraIOSBootcampProject/BaseTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  BaseTabBarController.swift
+//  SimpraIOSBootcampProject
+//
+//  Created by Muhammed Gül on 13.01.2023.
+//
+
+import UIKit
+
+class BaseTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+}
---
+++ SimpraIOSBootcampProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpraIOSBootcampProject
+//
+//  Created by Muhammed Gül on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
