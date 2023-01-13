98819fdee1e78ec72f81291792dd1fdfd45d0ef6
MuhammedGul34 <muhammedgul.mail@gmail.com>
2023-01-13 10:54:41 +0300

    Creating TabbarController

--- SimpraIOSBootcampProject/BaseTabBarController.swift
+++ SimpraIOSBootcampProject/BaseTabBarController.swift
@@ -11,6 +11,28 @@ class BaseTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .yellow
+        let redViewController = UIViewController()
+        redViewController.view.backgroundColor = .white
+        redViewController.navigationItem.title = "GAMES"
+        
+        let redNavController = UINavigationController(rootViewController: redViewController)
+        redNavController.tabBarItem.title = "RED NAV"
+        redNavController.tabBarItem.image = UIImage(named: "apps")
+        redNavController.navigationBar.prefersLargeTitles = true
+        
+        let blueViewController = UIViewController()
+        blueViewController.view.backgroundColor = .white
+        blueViewController.navigationItem.title = "SEARCH"
+        
+        let blueNavController = UINavigationController(rootViewController: blueViewController)
+        blueNavController.tabBarItem.title = "BLUE NAV"
+        blueNavController.tabBarItem.image = UIImage(named: "search")
+        blueNavController.navigationBar.prefersLargeTitles = true
+        
+        
+        viewControllers = [
+        redNavController,
+        blueNavController
+        ]
     }
 }
