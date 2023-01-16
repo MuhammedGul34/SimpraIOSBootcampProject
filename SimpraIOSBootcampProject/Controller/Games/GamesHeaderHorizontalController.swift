8ab43bb9ca8b221a25854a2ceda75aa1279502df
MuhammedGul34 <muhammedgul.mail@gmail.com>
2023-01-16 19:18:04 +0300

    İmplementingCollectionHeader

---
+++ SimpraIOSBootcampProject/Controller/Games/GamesHeaderHorizontalController.swift
@@ -0,0 +1,40 @@
+//
+//  GamesHeaderHorizontalController.swift
+//  SimpraIOSBootcampProject
+//
+//  Created by Muhammed Gül on 16.01.2023.
+//
+
+import UIKit
+
+class GamesHeaderHorizontalController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let cellId = "cellId"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.register(GameHeaderCell.self, forCellWithReuseIdentifier: cellId)
+        
+        if let layout = collectionViewLayout as? UICollectionViewFlowLayout {
+            layout.scrollDirection = .horizontal
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width - 48, height: view.frame.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return .init(top: 0, left: 16, bottom: 0, right: 0)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+}
