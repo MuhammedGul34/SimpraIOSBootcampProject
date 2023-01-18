bc0859f6edd025edb75a97a2911702503f120623
MuhammedGul34 <muhammedgul.mail@gmail.com>
2023-01-18 23:25:30 +0300

    HandledUserSelection

---
+++ SimpraIOSBootcampProject/Controller/Games/Details/GameDetailController.swift
@@ -0,0 +1,15 @@
+//
+//  GameDetailController.swift
+//  SimpraIOSBootcampProject
+//
+//  Created by Muhammed Gül on 18.01.2023.
+//
+
+import UIKit
+
+class GameDetailController: BaseListController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView?.backgroundColor = .yellow
+    }
+}
--- SimpraIOSBootcampProject/Controller/Games/GameHorizontalController.swift
+++ SimpraIOSBootcampProject/Controller/Games/GameHorizontalController.swift
@@ -14,7 +14,16 @@ class GameHorizontalController: HorizontalSnappingController, UICollectionViewDe
     var gameGroup: SearchResults?
     
     var socialApps = [SocialApp]()
+    
+    
 
+    var didSelectHandler: ((Result) -> ())?
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if let app = gameGroup?.results[indexPath.item] {
+            didSelectHandler?(app)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +37,9 @@ class GameHorizontalController: HorizontalSnappingController, UICollectionViewDe
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return gameGroup?.results.count ?? 0
     }
+    
+    
+
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! GameRowCell
