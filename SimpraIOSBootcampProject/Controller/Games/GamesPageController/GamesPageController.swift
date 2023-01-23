8a8249fa64ba36237293a268fcdcd950da97983c
MuhammedGul34 <muhammedgul.mail@gmail.com>
2023-01-23 16:32:19 +0300

    completingLocalNotification

--- SimpraIOSBootcampProject/Controller/Games/GamesPageController/GamesPageController.swift
+++ SimpraIOSBootcampProject/Controller/Games/GamesPageController/GamesPageController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
-
-
 class GamesPageController: BaseListController, UICollectionViewDelegateFlowLayout {
+    
+    let notificationCenter = UNUserNotificationCenter.current()
     
     let cellId = "id"
     let headerId = "headerId"
@@ -23,6 +23,17 @@ class GamesPageController: BaseListController, UICollectionViewDelegateFlowLayou
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        notificationCenter.requestAuthorization(options: [.sound,.badge,.alert]) { success, error in
+                    if error == nil {
+                        print("Authorization Successfuly")
+                    } else {
+                        print("Authorization Failed")
+                    }
+                }
+
+        localNotification()
+
         collectionView.register(GamesGroupCell.self, forCellWithReuseIdentifier: cellId)
         
         collectionView.register(GamesPageHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: headerId)
@@ -138,4 +149,25 @@ class GamesPageController: BaseListController, UICollectionViewDelegateFlowLayou
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return .init(top: 16, left: 0, bottom: 0, right: 0)
     }
+    
+    func localNotification(){
+           //MARK: Crate Trigger
+           
+           let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+           
+           let content = UNMutableNotificationContent()
+           content.title = "Hey Gamer"
+           content.body = "Search tons of game you are looking for!"
+           content.sound = UNNotificationSound.default
+           
+           let identifier = UUID().uuidString
+           
+           let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+           
+           notificationCenter.add(request) { error in
+               if error == nil {
+                   print("Message Sent Successfully")
+               }
+           }
+       }
 }
